2c72d58bbd05f8a3c139efcf95d0a8e4165861de
SONG <geun2121@gmail.com>
2025-08-01 23:26:00 +0900

    Entities 추가

---
+++ VolumeAR/VolumeAR/Entities/SurfaceInfo.swift
@@ -0,0 +1,14 @@
+//
+//  SurfaceInfo.swift
+//  VolumeAR
+//
+//  Created by SONG on 8/1/25.
+//
+
+import ARKit
+
+struct SurfaceInfo {
+    let position: simd_float3
+    let normal: simd_float3
+    let distance: Float
+}
