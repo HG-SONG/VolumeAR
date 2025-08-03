026c1d77c6ac02c755d34db5135e88f0eb497e07
SONG <geun2121@gmail.com>
2025-08-03 14:21:09 +0900

    frame 변화 업데이트

--- VolumeAR/VolumeAR/SurfaceTracker.swift
+++ VolumeAR/VolumeAR/SurfaceTracker.swift
@@ -10,6 +10,10 @@ import Combine
 
 final class SurfaceTracker: NSObject, ARSCNViewDelegate {
     let modePublisher = CurrentValueSubject<Mode, Never>(.searching)
+    private let cameraTransformSubject = PassthroughSubject<simd_float4x4, Never>()
+    var cameraTransformPublisher: AnyPublisher<simd_float4x4, Never> {
+        cameraTransformSubject.eraseToAnyPublisher()
+    }
     
     private func updateMode(for sceneView: ARSCNView) {
         let center = CGPoint(x: sceneView.bounds.midX, y: sceneView.bounds.midY)
@@ -25,5 +29,9 @@ final class SurfaceTracker: NSObject, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         guard let cameraView = renderer as? ARSCNView else { return }
         Task { @MainActor in updateMode(for: cameraView) }
+        
+        if let frame = cameraView.session.currentFrame {
+            cameraTransformSubject.send(frame.camera.transform)
+        }
     }
 }
