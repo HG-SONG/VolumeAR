2f08100a43e86d5019687700be22972c40c04bbb
SONG <geun2121@gmail.com>
2025-08-03 14:29:48 +0900

    HapticManager 추가
    - 수평정렬이 되었을 때, 임팩트 피드백을 발생시킵니다.

---
+++ VolumeAR/VolumeAR/HapticManager.swift
@@ -0,0 +1,35 @@
+//
+//  HapticManager.swift
+//  VolumeAR
+//
+//  Created by SONG on 8/2/25.
+//
+
+import UIKit
+
+final class HapticManager {
+    static let shared = HapticManager()
+    
+    private let impactGenerator = UIImpactFeedbackGenerator(style: .medium)
+    private let threshold: CGFloat
+    private var wasOffsetOutsideThreshold = false
+    
+    private init(threshold: CGFloat = 0.05) {
+        self.threshold = threshold
+        impactGenerator.prepare()
+    }
+    
+    func impactIfNeeded(offset: CGFloat) {
+        let isInside = abs(offset) < threshold
+        
+        if isInside && wasOffsetOutsideThreshold {
+            impactGenerator.impactOccurred()
+        }
+        
+        wasOffsetOutsideThreshold = !isInside
+    }
+    
+    func triggerImpact() {
+        impactGenerator.impactOccurred()
+    }
+}
